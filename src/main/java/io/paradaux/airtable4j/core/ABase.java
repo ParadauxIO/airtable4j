452d54bb1db0c72092991646d45ef6e678433043
Rían Errity <rian@paradaux.io>
2021-07-22 18:46:27 +0100

    Base API (WIP) - Ability to get URL/Table object

--- src/main/java/io/paradaux/airtable4j/core/ABase.java
+++ src/main/java/io/paradaux/airtable4j/core/ABase.java
@@ -1,14 +1,27 @@
 package io.paradaux.airtable4j.core;
 
 import io.paradaux.airtable4j.Airtable4J;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ABase {
 
-    private final String baseName;
+    private static final Logger logger = LoggerFactory.getLogger(ABase.class);
+    private static final String BASE_URL_ENDPOINT = "https://api.airtable.com/v0/%s/";
+
+    private final String baseID;
     private final Airtable4J airtable;
 
-    public ABase(String baseName, Airtable4J airtable) {
-        this.baseName = baseName;
+    public ABase(String baseID, Airtable4J airtable) {
+        this.baseID = baseID;
         this.airtable = airtable;
+    }
+
+    public String url() {
+        return String.format(BASE_URL_ENDPOINT, baseID);
+    }
+
+    public ATable table(String name) {
+        return new ATable(name, airtable, this);
     }
 }
