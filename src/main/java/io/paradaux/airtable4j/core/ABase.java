0b347c45774b3d8eb952c9bbe2c5798c7f90b291
Rían Errity <rian@paradaux.io>
2021-07-22 14:12:23 +0100

    Base impl as POJO

--- src/main/java/io/paradaux/airtable4j/core/ABase.java
+++ src/main/java/io/paradaux/airtable4j/core/ABase.java
@@ -1,4 +1,14 @@
 package io.paradaux.airtable4j.core;
 
+import io.paradaux.airtable4j.Airtable4J;
+
 public class ABase {
+
+    private final String baseName;
+    private final Airtable4J airtable;
+
+    public ABase(String baseName, Airtable4J airtable) {
+        this.baseName = baseName;
+        this.airtable = airtable;
+    }
 }
