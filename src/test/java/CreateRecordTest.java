d243a8897be2b6ef75c9138ac6d54179abf817a8
Rían Errity <rian@paradaux.io>
2021-07-24 13:43:03 +0100

    Clean up test suite

--- src/test/java/CreateRecordTest.java
+++ src/test/java/CreateRecordTest.java
@@ -1,13 +1,17 @@
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
 import io.paradaux.airtable4j.Airtable4J;
+import io.paradaux.airtable4j.core.structure.ABase;
 import object.Post;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,68 +24,31 @@ public class CreateRecordTest {
     private final String BASE_ID = System.getenv("airtable-base-id");
 
     @Test
-    public void temp() {
+    public void createRecordTest() throws InterruptedException {
+        if (API_KEY == null || BASE_ID == null) return;
+
         Airtable4J airtable4J = new Airtable4J(API_KEY);
+        ABase base = airtable4J.base(BASE_ID);
 
         List<Post> posts = new ArrayList<>();
         posts.add(new Post("345345", "https://example.org", "Submitted", "wefweffwe"));
         posts.add(new Post("343455345", "https://example.org/ergerg", "Submitted", "wefwergergeffwe"));
 
-        JsonArray fields = new JsonArray();
-        for (Object o : posts) {
-            JsonObject record = new JsonObject();
-            record.add("fields", gson.toJsonTree(o));
-            fields.add(record);
-        }
+        base.table("Queue").create(posts, new Callback() {
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                System.out.println("Response Code: " + response.code());
+                System.out.println("response: " + response.body().string());
+            }
 
-        JsonObject records = new JsonObject();
-        records.add("records", fields);
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                System.out.println("executed: " + call.isExecuted());
+                System.out.println("error: " + e.getMessage());
+            }
+        });
 
-        System.out.println(gson.toJson(records));
+        Thread.sleep(5000);
     }
-
-
-//    @Test
-//    public void createRecordTest() throws InterruptedException {
-//        Airtable4J airtable4J = new Airtable4J(API_KEY);
-//        ABase base = airtable4J.base(BASE_ID);
-//
-//        Map<String, Map<String, String>> fieldsObject = new HashMap<>();
-//        List<Map<String, Map<String, String>>> listOfFields = new ArrayList<>();
-//
-//        List<Post> posts = new ArrayList<>();
-//
-//
-//        HashMap<String, String> fields = new HashMap<>();
-//        fields.put("ID", "69");
-//        fields.put("Link", "https://example.org");
-//        fields.put("Status", "Submitted");
-//        fields.put("Post", "This is a test.");
-//
-//        fieldsObject.put("fields", fields);
-//
-//        listOfFields.add(fieldsObject);
-//
-//        System.out.println(API_KEY + ":" + BASE_ID);
-//
-//        Call call = base.table("Queue").create(listOfFields, new Callback() {
-//            @Override
-//            public void onFailure(@NotNull Call call, @NotNull IOException e) {
-//                System.out.println("executed: " + call.isExecuted());
-//                System.out.println(call.request().url());
-//                System.out.println();
-//            }
-//
-//            @Override
-//            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
-//                System.out.println("executed: " + call.isExecuted());
-//                System.out.println(call.request().url());
-//                System.out.println("response: " + response.body().string());
-//            }
-//        });
-//
-//        System.out.println(call.isExecuted());
-//        Thread.sleep(5000);
-//    }
 
 }
